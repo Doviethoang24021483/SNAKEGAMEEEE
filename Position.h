cda6f621c269efbd573c5450f3c4319dad9272fd
Doviethoang24021483 <24021483@vnu.edu.vn>
2025-03-13 23:43:38 +0700

    first commit

--- Position.h
+++ Position.h
@@ -6,12 +6,22 @@ class Position{
   public:
      //Ham khoi tao voi gia tri mac dinh
       Position(int x_=0, int y_=0);
-      //Kiem tra xem vi tri co nam trong san ko
+
+      // Ham lay toa do x
       int getx() const;
+
+      //ham lay toa do y
       int gety() const;
-      int setx(int newx) ;
-      int sety(int newy) ;
-      bool isInsideBox(int left,int right,int width,int height );
+
+      //Ham tra ve doi tuong Position moi voi x da thay doi
+      Position setx(int newx) const;
+
+      //Ham tra ve doi tuong Position moi voi y da thay doi
+      Position sety(int newy) const;
+
+      //Kiem tra xem vi tri co nam trong hcn
+      bool isInsideBox(int left,int top,int width,int height );
+
       //In ra vi tri
       void disPlay() const;
 };
