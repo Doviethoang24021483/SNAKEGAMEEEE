2b830a755c5f8e94c909d5577ebc290a89a11679
Doviethoang24021483 <24021483@vnu.edu.vn>
2025-03-13 23:44:15 +0700

    first commit

--- Position.cpp
+++ Position.cpp
@@ -2,24 +2,31 @@
 #include "Position.h"
 #include<iostream>
 using namespace std;
+
 Position::Position(int x_, int y_):x(x_),y(y_){}
+
 bool Position::isInsideBox(int left, int top, int w, int h){
- if(x>=left && x<left+w&& x>=top&&x<top+h) return true;
+ if(x>=left && x<left+w&& y>=top&&y<top+h) return true;
 
  return false;
 }
+
 int Position::getx() const{
   return x;
 }
-int Position::gety() const{
+
+int Position::gety() const {
   return y;
 }
-int Position::setx(int newx){
-x= newx;
+
+Position Position::setx(int newx) const {
+ return Position(newx,y);
 }
-int Position::sety(int newy){
-y=newy;
+//Tao ra 1 doi tuong Position moi voi y da dc thay doi an toan, de go loi, de bao tri
+Position Position::sety(int newy) const {
+ return Position(x,newy);
 }
+
 void Position::disPlay() const{
  cout<<x<<" "<<y;
 }
