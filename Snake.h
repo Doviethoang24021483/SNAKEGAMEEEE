bd751b357fb277b303d4ca2b44f4f165dc9b1ef1
Doviethoang24021483 <24021483@vnu.edu.vn>
2025-03-13 23:42:29 +0700

    first commit

--- Snake.h
+++ Snake.h
@@ -10,6 +10,7 @@ enum Direction{
   RIGHT,
   LEFT
 };
+
 enum UserInput{
   NO_INPUT,
   KEY_UP,
@@ -22,14 +23,22 @@ class Snake{
  private:
      Direction direction;
      PlayGround* playGround;
-     Position pos;
+     vector<Position> body;
      queue<UserInput> inputQueue;
  public:
+    //Nhan mot con tro tro den doi tuong playground de tuong tac
     Snake(PlayGround* playGround_);
+    //Them input tu nguoi choi vao hang doi
     void processUserInput (UserInput input);
+    //Thay doi huong
     Direction changeDirection(UserInput input);
-    void nextStep();
-    Position getPosition()const;
+    //Di chuyen ran 1 buoc
+    bool nextStep();
+
     Direction getDirection() const;
+    // Xu li khi an cherry
+    void eatCherry();
+    //Tra ve vi tri cua tung dot ran.
+    const vector<Position>& getBody() const;
 };
 #endif // SNAKE_H
