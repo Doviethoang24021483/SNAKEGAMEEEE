91061b6a8523a7f0e00b9fb01544990865047e1b
Doviethoang24021483 <24021483@vnu.edu.vn>
2025-03-13 23:41:47 +0700

    first commit

--- PlayGround.cpp
+++ PlayGround.cpp
@@ -1,38 +1,78 @@
 
 #include<iostream>
-#include "Playground.h"
+#include "PlayGround.h"
+#include "Position.h"
 #include<cassert>
 #include<vector>
+#include<cstdlib>
+#include<ctime>
 using namespace std;
 
- PlayGround::PlayGround(int width, int height)
-  :width(width),
-  height (height),
-   squares (height,vector<CellType>(width, CELL_EMPTY)),
-    status(GAME_RUNNING),
-    isStop (false)
-            {
-            }
- int PlayGround::getWidth() const{
-  return squares[0].size();
+PlayGround::PlayGround(int width_, int height_)
+: width(width_),height(height_),
+squares(height,vector<CellType>(width,CELLEMPTY)),
+status(GAMERUNNING),
+gameRun(true),
+cherry(0,0)
+{
+ srand(time(0));
+ generateCherry({}); //tao vi tri ngau nhien
+}
+//Tao ra vi tri cherry ngau nhien
+void PlayGround::generateCherry(const vector<Position>& snakeBody) {
+ Position newCherry;
+ bool validPosition = false;
+ while(!validPosition){
+    int x = rand() % getWidth();
+    int y = rand() % getHeight();
+    newCherry=Position(x,y);
+    validPosition=true;
+    for(const Position& segment: snakeBody){
+        if(segment.getx() == newCherry.getx() && segment.gety()==newCherry.gety()){
+            validPosition = false;
+            break;
+        }
+    }
  }
- int PlayGround::getHeight() const{
-  return squares.size();
- }
+ setCherry(newCherry);
+}
 
- CellType PlayGround::getCellState(Position pos) const{
-   assert(pos.isInsideBox(0,0,getWidth(),getHeight()));
-   return squares[pos.y][pos.x];
- }
- int PlayGround::changeCellState(Position pos, CellType type){
-  assert(pos.isInsideBox(0,0,getWidth(),getHeight()));
-  return squares[pos.y][pos.x]=type;
- }
- bool PlayGround::checkPosition(Position pos){
-  if(!pos.isInsideBox(0,0,getWidth(),getHeight()))
-    return false;
-  return true;
- }
- bool PlayGround::isGameRunning() const{
-   return status == GAME_RUNNING && !isStop;
- }
+int PlayGround::getWidth() const {
+    return width;
+}
+
+int PlayGround::getHeight() const{
+    return height;
+}
+
+Position PlayGround::getCherry() const{
+   return cherry;
+}
+
+CellType PlayGround::getCellType(Position pos)
+{ assert(pos.isInsideBox(0,0,getWidth(),getHeight()));
+return squares[pos.gety()][pos.getx()];
+}
+
+CellType PlayGround::changeCellType(CellType type, Position pos){
+    if(pos.isInsideBox(0,0,getWidth(),getHeight()))
+    squares[pos.getx()][pos.gety()]=type;
+    return squares[pos.gety()][pos.getx()];
+}
+
+bool PlayGround::isGameRun()const{
+ return status == GAMERUNNING && gameRun;
+}
+
+void PlayGround::setCherry(Position newCherry){
+  cherry=newCherry;
+}
+
+void PlayGround::stopGame(){
+ gameRun = false;
+ status = GAMESTOP;
+}
+
+void PlayGround::setGameRun(bool run){
+  gameRun=run;
+}
