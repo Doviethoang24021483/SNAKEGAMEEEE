8df54a136dbb0a016c0aec7d7154f2a5fb9d44b4
Doviethoang24021483 <24021483@vnu.edu.vn>
2025-03-14 14:40:10 +0700

    first commit

--- SDL_utils.h
+++ SDL_utils.h
@@ -1,20 +1,23 @@
+
 #ifndef SDL_UTILS_H
 #define SDL_UTILS_H
 
 
 #include <SDL.h>
+#include <SDL_image.h>
 #include<SDL_image.h>
 #include<string>
 using namespace std;
 
 void logSDLError(std::ostream& os,
 const std::string &msg, bool fatal);
-const int SCREEN_WIDTH = 800;
+const int SCREEN_WIDTH = 600;
  const int SCREEN_HEIGHT = 600;
  const string WINDOW_TITLE =
 "An Implementation of Code.org Painter";
+SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren);
 void initSDL(SDL_Window* &window, SDL_Renderer* &renderer);
- void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer, SDL_Texture* snakeTexture, SDL_Texture* snakeHeadTexture, SDL_Texture* cherryTexture);
  void waitUntilKeyPressed();
 #endif // SDL_UTILS_H
 
