e0b2120282db6058ba82e07194c3a8b997772f5b
Doviethoang24021483 <24021483@vnu.edu.vn>
2025-03-20 00:30:02 +0700

    first commit

--- PlayGround.h
+++ PlayGround.h
@@ -3,13 +3,14 @@
 #include<vector>
 #include "Position.h"
 using namespace std;
+
 //Dinh nghia cac loai o co the co tren san choi
 enum CellType{
  CELLEMPTY,
  CELLSNAKE,
- CELLCHERRY,
- CELLWALL
+ CELLCHERRY
 };
+
 //Dinh nghia cac trang thai co the co
 enum GameStatus{
  GAMERUNNING,
@@ -17,29 +18,41 @@ enum GameStatus{
  GAMELOST,
  GAMESTOP
 };
+
+struct CellSize {
+    int width;
+    int height;
+};
+
 class PlayGround {
 private:
  int diemso;
  int width, height;
  GameStatus status;
  vector<vector<CellType>> squares;
- Position cherry;
+ //Position cherry;
+ vector<Position> notes; // Thêm dòng này
+ const int NUM_NOTES = 20; // Thêm dòng này
  bool gameRun;
- vector<Position> walls;//Danh sach vi tri cac buc tuong
+ //vector<Position> walls;//Danh sach vi tri cac buc tuong
 public:
     PlayGround(int width_=0, int height_=0);
     int getWidth() const;
     int getHeight() const;
-    Position getCherry() const;
-    void setCherry(Position newCherry);
+    //Position getCherry() const;
+    //void setCherry(Position newCherry);
+    const vector<Position>& getNotes() const; // Thêm dòng này
     CellType getCellType(Position pos);
     CellType changeCellType(CellType type, Position pos );
     bool isGameRun() const;
-    void generateCherry(const vector<Position>& snakeBody);
+    //void generateCherry(const vector<Position>& snakeBody);
     void stopGame();
     void setGameRun(bool run);
     const vector<Position>& getWalls() const;
-    void addWall(int x, int y);
+    //void addWall(int x, int y);
+    void generateNotes(const vector<Position>& snakeBody);
+ void generateNewNote(const vector<Position>& snakeBody);
+ void removeNote(Position note);
 };
 #endif // PLAYGROUND_H
 
