4944d44250b0a3f23a40c2fd0a978911ae610e1d
Doviethoang24021483 <24021483@vnu.edu.vn>
2025-03-24 08:30:25 +0700

    first commit

--- Snake.cpp
+++ Snake.cpp
@@ -5,7 +5,9 @@
 #include<iostream>
 #include<cmath>
 #include "AudioManager.h"
+
 const CellSize CELL_SIZE = {24,24};
+
 using namespace std;
 Snake::Snake(PlayGround* playGround_, AudioManager* audioManager_)
 : playGround(playGround_),
@@ -21,6 +23,7 @@ Snake::Snake(PlayGround* playGround_, AudioManager* audioManager_)
       if(input!=UserInput::NO_INPUT)
     inputQueue.push(input);
   }
+
   Direction Snake::changeDirection(UserInput input){
     if (input == UserInput::KEY_UP && direction != Direction::DOWN) return UP;
     if (input == UserInput::KEY_DOWN && direction != Direction::UP) return DOWN;
@@ -28,15 +31,15 @@ Snake::Snake(PlayGround* playGround_, AudioManager* audioManager_)
     if (input == UserInput::KEY_RIGHT && direction != Direction::LEFT) return RIGHT;
     return direction; // Giữ nguyên hướng nếu input không hợp lệ
 }
+
   bool Snake::nextStep(){
       bool ateNote=false;
-      cout << "Giá trị của ateNote trước vòng lặp: " << ateNote << endl; // Thêm dòng này
 
       if(!inputQueue.empty())
       {
-          UserInput input=inputQueue.front();
-          inputQueue.pop();
-          direction=changeDirection(input);
+          UserInput input=inputQueue.front(); // lay lenh dau tien ra
+          inputQueue.pop(); // xoa lenh do
+          direction=changeDirection(input); // Thay doi huong
       }
       int newX=body.front().getx();
       int newY=body.front().gety();
@@ -51,81 +54,60 @@ Snake::Snake(PlayGround* playGround_, AudioManager* audioManager_)
       int piNewX = newX*CELL_SIZE.width;
       int piNewY = newY*CELL_SIZE.height;
       Position piNewHead(piNewX,piNewY);
-int boundaryX = playGround->getWidth() * CELL_SIZE.width;
-int boundaryY = playGround->getHeight() * CELL_SIZE.height;
 
-if (!piNewHead.isInsideBox(0, 0,SCREEN_WIDTH  ,SCREEN_HEIGHT )) {
+if (!piNewHead.isInsideBox(0, 0,SCREEN_WIDTH, SCREEN_HEIGHT )) {
     playGround->stopGame();
     return false;
 }
 
+   // Kiem tra va cham voi than ran
   for(int i=1;i<body.size();i++){
     if(newHead.getx()==body[i].getx()&&newHead.gety()==body[i].gety()){
         playGround->stopGame();
         return false;
     }
   }
-  //Kiem tra va cham voi tuong
-  // const vector<Position>& walls = playGround->getWalls();
-   // for (const Position& wall : walls) {
-      //  if (newHead.getx() == wall.getx() && newHead.gety() == wall.gety()) {
-         //   playGround->stopGame();
-          //  return false;
-       // }
-   // }
-  //if(newHead.getx()==playGround->getCherry().getx()&&newHead.gety()==playGround->getCherry().gety()){
-   // eatCherry();
-    //playGround->generateCherry(body);
-   // ateCherry=true;
- // }
-
 
   const vector<Position>& notes = playGround->getNotes();
     for (const Position& note : notes) {
-             cout << "Tọa độ nốt nhạc: (" << note.getx() << ", " << note.gety() << ")" << endl; // Thêm dòng
         if (newHead.getx() == note.getx() && newHead.gety() == note.gety()) {
-            eatNote();
             ateNote = true;
             break; // Ăn một nốt nhạc thì thoát khỏi vòng lặp
         }
     }
 
-    cout << "Giá trị của ateNote sau vòng lặp: " << ateNote << endl; // Thêm dòng này
   body.insert(body.begin(),newHead);
   if(!ateNote){
     body.pop_back();
   }
-  //if(!ateCherry){
-   // body.pop_back();
-  //}
+
   return ateNote;
   }
-  //void Snake::eatCherry(){
-      //co the them diem so o day
-   // cout<<"Ran da an cherry"<<endl;
- // }
-   void Snake::eatNote() { //thay the ham nay
+
+   void Snake::eatNote() {
     //co the them diem so o day
-    cout<<"Ran da an not nhac"<<endl; // Thêm dòng này
+    cout<<"Ran da an not nhac"<<endl;
     Position head = body.front();
     for (size_t i = 0; i < playGround->getNotes().size(); ++i) {
-            Position note = playGround->getNotes()[i];
+            Position note = (playGround->getNotes())[i];
              if (head.getx() == note.getx() && head.gety() == note.gety()) {
                      audioManager->playRandomNoteChunk();
                     playGround->removeNote(note);
                     playGround->generateNewNote(body);
-                   // body.push_back(body.back());
                      break;
              }
     }
 
   }
+
   Direction Snake::getDirection() const {
       return direction;
   }
+
   const vector<Position>& Snake::getBody() const{
    return body;
   }
+
   double Snake::getRotationAngle() const {
     switch (direction) {
         case Direction::UP: return 90.0;
